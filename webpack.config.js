9157cb5db2cc45193f670b3f64d3c26ea071e56e
Lee yongjun <chuck5732590@gmail.com>
2022-08-28 16:52:05 +0900

    webpack config 수정

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./app.js",
+  entry: "./src/app.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.[chunkhash].js",
